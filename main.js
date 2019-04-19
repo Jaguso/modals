0fa3c5ba0c607068073f6c497141b1233a88f3bc
Jaguso <jagutzo@gmail.com>
2019-04-19 17:17:53 -0500

    button for closing modal works

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ document.getElementById('button').addEventListener('click',
 function() {
   document.querySelector('.bg-modal').style.display = 'flex';
 });
+
+document.querySelector('.close').addEventListener('click', 
+function() {
+  // console.log(123)
+  document.querySelector('.bg-modal').style.display = 'none';
+});
